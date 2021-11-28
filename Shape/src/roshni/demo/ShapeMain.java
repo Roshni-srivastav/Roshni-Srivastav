01b96a0efec1dd4fd80a276ae6eb3e8f3fbc3b7c
Roshni-srivastav <76032747+Roshni-srivastav@users.noreply.github.com>
2021-11-28 23:13:31 +0530

    Add files via upload

---
+++ Shape/src/roshni/demo/ShapeMain.java
@@ -0,0 +1,15 @@
+package roshni.demo;
+
+public class ShapeMain {
+
+	public static void main(String[] args) {
+		Shape shape=new Shape();
+
+		shape.area(9, 5);
+		shape.area(18);
+		shape.perimeter(7,15);
+		shape.perimeter(6);
+
+	}
+
+}
